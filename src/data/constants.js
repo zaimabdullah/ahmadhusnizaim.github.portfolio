a19c9fa79fb1684a4f4658e5e0f2b7404331b6a9
Husni Zaim <husnizaim97@gmail.com>
2024-08-20 19:33:24 +0800

    Update roles

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,11 +1,6 @@
 export const Bio = {
   name: 'Ahmad Husni Zaim Abdullah',
-  roles: [
-    'IT Student',
-    'Programmer',
-    'Future Web Developer',
-    'Future Software Engineer',
-  ],
+  roles: ['IT Student', 'Programmer'],
   description:
     'I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.',
   github: 'https://github.com/zaimabdullah',
