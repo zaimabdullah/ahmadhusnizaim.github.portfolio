601a0f4be636dfa8cdefacda0b02d596637d3f68
Ahmad Husni Zaim <husnizaim97@gmail.com>
2025-11-27 10:40:24 +0800

    Update resume GDrive share link

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     'I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.',
   github: 'https://github.com/zaimabdullah',
   resume:
-    'https://drive.google.com/file/d/1KzRml-VdXVSAMhyg2uIRa2nyxhpv44ur/view?usp=sharing',
+    'https://drive.google.com/file/d/1WLKNwrLp-xc3GUQgECq1fmk5Iih21Ke9/view?usp=sharing',
   linkedin: 'https://www.linkedin.com/in/husnizaim/',
   twitter: 'https://twitter.com/zcreative14',
   insta: 'https://www.instagram.com/ah_z1997/',
